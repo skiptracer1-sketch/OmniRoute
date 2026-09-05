1ecdefe4d49ec48c3ca2b15f93cd20ba891bc8fd
skiptracer1-sketch <skiptracer1@gmail.com>
2026-09-05 18:20:25 +0000

    test(swarm): expose blackboard through release package

---
+++ tests/lumexus-swarm/package-blackboard.test.ts
@@ -0,0 +1,1 @@
+import { expect, it } from 'vitest'; import { SwarmBlackboard } from '../../src/lumexus-swarm/package'; it('exports blackboard through package surface',()=>expect(SwarmBlackboard).toBeDefined());
